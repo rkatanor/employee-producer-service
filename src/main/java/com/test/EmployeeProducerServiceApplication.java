f8127035267d3575cf56000a4e1648d13719fd52
Rajkumar Katanor <rkatanor@valuelabs.com>
2019-05-05 02:36:39 -0700

    changed app to war

--- src/main/java/com/test/EmployeeProducerServiceApplication.java
+++ src/main/java/com/test/EmployeeProducerServiceApplication.java
@@ -2,12 +2,19 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeProducerServiceApplication {
+public class EmployeeProducerServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeProducerServiceApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(EmployeeProducerServiceApplication.class);
 	}
 
 }
